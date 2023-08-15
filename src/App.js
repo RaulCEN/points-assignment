51bc8024a1bf78b05937b06daa2a06a22bf5379c
Raul Canul <raul.canul@encora.com>
2023-08-15 12:20:54 -0600

    feat: react-query installed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { Input } from './components/Input';
-import { Button }  from './components/Button';
+import { Button } from './components/Button';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <section className='app--container'>
-      <h1 className='app--title'>Tax Calculator</h1>
+    <QueryClientProvider client={queryClient}>
+      <section className='app--container'>
+        <h1 className='app--title'>Tax Calculator</h1>
 
-      <div className='app--input-row'>
-        <Input placeholder='Annual Income' />
-        <Input placeholder='Tax year' />
-      </div>
+        <div className='app--input-row'>
+          <Input placeholder='Annual Income' />
+          <Input placeholder='Tax year' />
+        </div>
 
-      <Button>Calculate</Button>
-    </section>
+        <Button>Calculate</Button>
+      </section>
+    </QueryClientProvider>
   );
 }
 
