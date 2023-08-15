7554be2a546dc820132bb358e55553bf3366b44a
Raul Canul <raul.canul@encora.com>
2023-08-15 11:50:56 -0600

    feat: styles added to Input component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Input } from './components/Input';
+import { Button }  from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='app--container'>
+      <h1 className='app--title'>Tax Calculator</h1>
+
+      <div className='app--input-row'>
+        <Input placeholder='Annual Income' />
+        <Input placeholder='Tax year' />
+      </div>
+
+      <Button>Calculate</Button>
+    </section>
   );
 }
 
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export const Input = () => (<input />);
+import './Input.style.css'
+
+export const Input = (props) => (<input {...props} className='input' />);
