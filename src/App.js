add89f01f617f245aef5489b6d460814cf223f99
Raul Canul <raul.canul@encora.com>
2023-08-15 12:32:00 -0600

    feat: input events added on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { useState } from 'react';
 
 import { Input } from './components/Input';
 import { Button } from './components/Button';
@@ -7,17 +8,27 @@ import { Button } from './components/Button';
 const queryClient = new QueryClient()
 
 function App() {
+  const [annualIncomeValue, setAnnualIncome] = useState('');
+  const [taxYearValue, setTaxYear] = useState('');
+
+  const onChangeAnnualIncome = input => setAnnualIncome(input.target.value);
+  const onChangeTaxYear = input => setTaxYear(input.target.value);
+
+  const onCalculate = () => {
+
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <section className='app--container'>
         <h1 className='app--title'>Tax Calculator</h1>
 
         <div className='app--input-row'>
-          <Input placeholder='Annual Income' />
-          <Input placeholder='Tax year' />
+          <Input placeholder='Annual Income' onChange={onChangeAnnualIncome} />
+          <Input placeholder='Tax year' onChange={onChangeTaxYear} />
         </div>
 
-        <Button>Calculate</Button>
+        <Button onClick={onCalculate}>Calculate</Button>
       </section>
     </QueryClientProvider>
   );
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 
 import './Button.style.css'
 
-export const Button = ({children}) => (<button className='button'>{ children }</button>)
+export const Button = ({children, onClick}) => (<button className='button' onClick={onClick}>{ children }</button>)
